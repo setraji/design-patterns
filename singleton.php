edaaf7688b878d7b945d5f89d3beff14ffe6cc03
setraji <info@setraji.de>
2016-07-28 18:42:43 +0200

    Update singleton.php

--- singleton.php
+++ singleton.php
@@ -1,5 +1,33 @@
 <?php
 
-echo "hallo";
+class Logfile {
+    private static $instance = null;
+    private $logfile = null;
+
+    public function __construct() {
+        $this->logfile = fopen("logfile.txt", "a");
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null)
+        {
+            self::$instance = new Logfile();
+        }
+        return self::$instance;
+    }
+
+    public function write($message) {
+        $message = date("d.m.Y H:i:s - ", time()) . $message . "\n";
+        fputs($this->logfile, $message);
+    }
+
+    public function __destruct() {
+        fclose($this->logfile);
+    }
+}
+
+// test:
+$log = Logfile::getInstance();
+$log->write("log entry xyz");
 
 ?>
