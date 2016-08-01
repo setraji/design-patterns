376ec2ec343e4f32bdf09322cb37fc10b866774d
setraji <info@setraji.de>
2016-08-02 00:15:42 +0200

    Create adapter.php

---
+++ adapter.php
@@ -0,0 +1,47 @@
+<?php
+
+class MariaDb {
+  protected $animal;
+
+	public function __construct($animal_in) {
+		$this->animal = $animal_in;
+	}
+
+	public function getAnimal() {
+		return $this->animal;
+	}
+}
+
+class Oracle {
+	protected $animal;
+
+	public function __construct($animal_in) {
+		$this->animal = $animal_in;
+	}
+
+	public function getAnimal() {
+		return $this->animal;
+	}
+}
+
+class MariaDbOracleAdapter
+{
+	private $mariadb;
+	private $oracle;
+
+	public function __construct(MariaDb $mariadb_in, Oracle $oracle_in) {
+		$this->mariadb = $mariadb_in;
+		$this->oracle = $oracle_in;
+	}
+
+	public function getAnimals() {
+		return "Animal from MariaDB: " . $this->mariadb->getAnimal() . '<br/>' .
+		"Animal from Oracle: " . $this->oracle->getAnimal();
+	}
+}
+
+$mariadb = new MariaDb("Cats");
+$oracle = new Oracle("Dogs");
+$animals = new MariaDbOracleAdapter($mariadb, $oracle);
+
+echo $animals->getAnimals();
