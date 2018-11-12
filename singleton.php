68347261669f95485729fed983acb489401fdcdf
setraji <info@setraji.de>
2018-11-12 23:27:41 +0100

    Update singleton.php

--- singleton.php
+++ singleton.php
@@ -1,5 +1,11 @@
 <?php
 
+// Beispiel: Erstellung eines Logfiles
+// Es wird eine einzige global verfügbare Logfile-Datei benötigt. 
+// Diese soll, wenn sie noch nicht vorhanden ist, automatisch erstellt werden.
+// Die zweite Aufgabe ist Nachrichten mit einem aktuellen Zeitstempel an das Ende der Datei hinzuzufügen.
+
+// Die Klasse:
 class Logfile {
     private static $instance = null;
     private $logfile = null;
@@ -26,6 +32,13 @@ class Logfile {
     }
 }
 
-// test:
+// Test:
 $log = Logfile::getInstance();
 $log->write("log entry xyz");
+
+// Der Inhalt der logfile.txt-Datei:
+// 17.07.2016 20:26:54 - Logeintrag xyz
+// 17.07.2016 20:27:00 - Logeintrag zyx
+// 17.07.2016 20:27:02 - Logeintrag zxy
+// 17.07.2016 20:27:02 - Logeintrag yxz
+// 17.07.2016 20:27:03 - Logeintrag xyz
